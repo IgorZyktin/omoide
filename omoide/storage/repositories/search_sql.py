55ae3600651f6bfab62773ec2b3e72f69ebdabd3
IgorZyktin <nicord@yandex.ru>
2022-01-31 22:20:28 +0300

    feat(app): strict search

--- omoide/storage/repositories/search_sql.py
+++ omoide/storage/repositories/search_sql.py
@@ -12,7 +12,7 @@ SELECT count(*) AS total_items
 FROM items it
          RIGHT JOIN computed_tags ct ON ct.item_uuid = it.uuid
 WHERE owner_uuid IN (SELECT user_uuid FROM public_users)
-  AND ct.tags && :tags_include
+  AND ct.tags @> :tags_include
   AND NOT ct.tags && :tags_exclude;
 """
 
@@ -39,7 +39,7 @@ SELECT owner_uuid,
 FROM items it
          RIGHT JOIN computed_tags ct ON ct.item_uuid = it.uuid
 WHERE owner_uuid IN (SELECT user_uuid FROM public_users)
-  AND ct.tags && :tags_include
+  AND ct.tags @> :tags_include
   AND NOT ct.tags && :tags_exclude
 ORDER BY number LIMIT :limit OFFSET :offset;
 """
