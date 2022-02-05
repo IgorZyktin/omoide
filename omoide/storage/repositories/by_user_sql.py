8413806521b47779b6b44c7936388dabd0858569
IgorZyktin <nicord@yandex.ru>
2022-02-05 14:24:21 +0300

    feat(app): refactored by_user sql

--- omoide/storage/repositories/by_user_sql.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-"""SQL queries.
-"""
-COUNT_ITEMS_OF_PUBLIC_USER = """
-SELECT count(*) AS total_items
-FROM items
-WHERE owner_uuid = :owner_uuid
-  AND parent_uuid IS NULL;
-"""
