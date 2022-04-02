5f0e957172326fdac12f5fb54b2d03acf96bda19
IgorZyktin <nicord@yandex.ru>
2022-04-02 16:34:14 +0300

    feat: added auth views

---
+++ omoide/presentation/application/auth.py
@@ -0,0 +1,71 @@
+# -*- coding: utf-8 -*-
+"""Auth related routes.
+"""
+
+import fastapi
+from fastapi.security import HTTPBasicCredentials, HTTPBasic
+from starlette import status
+
+from omoide import domain, use_cases
+from omoide.presentation import dependencies, infra, constants
+from omoide.presentation.config import config
+
+router = fastapi.APIRouter()
+security = HTTPBasic(realm='omoide')
+
+
+@router.get('/login')
+async def login(
+        request: fastapi.Request,
+        user: domain.User = fastapi.Depends(dependencies.get_current_user),
+        credentials: HTTPBasicCredentials = fastapi.Depends(security),
+        use_case: use_cases.AuthUseCase = fastapi.Depends(
+            dependencies.get_auth_use_case,
+        ),
+        response_class=fastapi.responses.RedirectResponse,
+):
+    """Ask user for login and password."""
+    url = request.url_for('search')
+
+    if not user.is_anon():
+        # already logged in
+        return fastapi.responses.RedirectResponse(url)
+
+    new_user = await use_case.execute(credentials)
+
+    if new_user.is_anon():
+        raise fastapi.HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail='Incorrect login or password',
+            headers={'WWW-Authenticate': 'Basic realm="omoide"'},
+        )
+
+    return fastapi.responses.RedirectResponse(url)
+
+
+@router.get('/logout')
+async def logout(
+        request: fastapi.Request,
+        response_class=fastapi.responses.HTMLResponse,
+):
+    """Clear authorization."""
+    details = infra.parse.details_from_params(
+        params=request.query_params,
+        items_per_page=constants.ITEMS_PER_PAGE,
+    )
+
+    query = infra.query_maker.from_request(request.query_params)
+
+    context = {
+        'request': request,
+        'config': config,
+        'user': domain.User.new_anon(),
+        'url': request.url_for('search'),
+        'query': infra.query_maker.QueryWrapper(query, details),
+    }
+
+    return dependencies.templates.TemplateResponse(
+        name='logout.html',
+        context=context,
+        status_code=401,
+    )
--- omoide/presentation/dependencies.py
+++ omoide/presentation/dependencies.py
@@ -1,7 +1,12 @@
 # -*- coding: utf-8 -*-
 """External components.
 """
+import binascii
+from base64 import b64decode
+
+import fastapi
 from databases import Database
+from fastapi.security import HTTPBasicCredentials
 from starlette.templating import Jinja2Templates
 
 from omoide import use_cases
@@ -23,20 +28,51 @@ browse_use_case = use_cases.BrowseUseCase(browse_repository)
 by_user_repository = repositories.ByUserRepository(db)
 by_user_use_case = use_cases.ByUserUseCase(by_user_repository)
 
+base_repository = repositories.BaseRepository(db)
+auth_use_case = use_cases.AuthUseCase(base_repository)
+
 templates = Jinja2Templates(directory='omoide/presentation/templates')
 
 
-def get_current_user():
+def get_credentials(
+        request: fastapi.Request,
+) -> HTTPBasicCredentials:
+    """Try extraction credentials from user request, but not trigger login."""
+    authorization: str = request.headers.get('Authorization')
+    anon = HTTPBasicCredentials(username='', password='')
+
+    if authorization:
+        scheme, _, param = authorization.partition(' ')
+
+        if scheme.lower() == 'basic':
+            try:
+                data = b64decode(param).decode('ascii')
+            except (ValueError, UnicodeDecodeError, binascii.Error):
+                return anon
+
+            username, separator, password = data.partition(':')
+
+            if not separator:
+                return anon
+
+            return HTTPBasicCredentials(username=username, password=password)
+
+    return anon
+
+
+def get_auth_use_case():
+    """Get use case instance."""
+    return auth_use_case
+
+
+async def get_current_user(
+        credentials: HTTPBasicCredentials = fastapi.Depends(get_credentials),
+        use_case: use_cases.AuthUseCase = fastapi.Depends(get_auth_use_case),
+) -> auth.User:
     """Load current user or use anon user."""
-    return auth.User(
-        uuid='',
-        login='anon',
-        password='',
-        name='anon',
-        visibility=None,
-        language=None,
-        last_seen=None,
-    )
+    if not credentials.username or not credentials.password:
+        return auth.User.new_anon()
+    return await use_case.execute(credentials)
 
 
 def get_search_use_case():
