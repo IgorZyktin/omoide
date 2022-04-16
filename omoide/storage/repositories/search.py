c45667bd7223f3b923192bd2675cda2c1af37751
IgorZyktin <nicord@yandex.ru>
2022-04-16 23:08:13 +0300

    feat: added search for authorised user

--- omoide/storage/repositories/search.py
+++ omoide/storage/repositories/search.py
@@ -111,8 +111,19 @@ class SearchRepository(
             user: domain.User,
     ) -> int:
         """Count all available items for authorised user."""
-        # TODO(i.zyktin): need to implement this
-        return await self.total_random_anon()
+        query = """
+        SELECT count(*) AS total_items
+        FROM items it
+                RIGHT JOIN computed_permissions cp ON cp.item_uuid = it.uuid
+        WHERE :user_uuid = ANY(cp.permissions);
+        """
+
+        values = {
+            'user_uuid': user.uuid,
+        }
+
+        response = await self.db.fetch_one(query, values)
+        return int(response['total_items'])
 
     async def total_specific_known(
             self,
@@ -120,8 +131,22 @@ class SearchRepository(
             query: domain.Query,
     ) -> int:
         """Count available items for authorised user."""
-        # TODO(i.zyktin): need to implement this
-        return await self.total_random_anon()
+        query = """
+        SELECT count(*) AS total_items
+        FROM items it
+                RIGHT JOIN computed_tags ct ON ct.item_uuid = it.uuid
+                RIGHT JOIN computed_permissions cp ON cp.item_uuid = it.uuid
+        WHERE :user_uuid = ANY(cp.permissions)
+          AND ct.tags @> :tags_include
+          AND NOT ct.tags && :tags_exclude;
+        """
+
+        values = {
+            'user_uuid': user.uuid,
+        }
+
+        response = await self.db.fetch_one(query, values)
+        return int(response['total_items'])
 
     async def search_random_known(
             self,
@@ -130,8 +155,30 @@ class SearchRepository(
             details: domain.Details,
     ) -> list[domain.Item]:
         """Find random items for authorised user."""
-        # TODO(i.zyktin): need to implement this
-        return await self.search_random_anon(query, details)
+        _query = """
+        SELECT uuid,
+               parent_uuid,
+               owner_uuid,
+               number,
+               name,
+               is_collection,
+               content_ext,
+               preview_ext,
+               thumbnail_ext
+        FROM items it
+            RIGHT JOIN computed_permissions cp ON cp.item_uuid = it.uuid
+        WHERE :user_uuid = ANY(cp.permissions)
+        ORDER BY random() LIMIT :limit OFFSET :offset;
+        """
+
+        values = {
+            'user_uuid': user.uuid,
+            'limit': details.items_per_page,
+            'offset': details.offset,
+        }
+
+        response = await self.db.fetch_all(_query, values)
+        return [domain.Item.from_map(row) for row in response]
 
     async def search_specific_known(
             self,
@@ -140,5 +187,33 @@ class SearchRepository(
             details: domain.Details,
     ) -> list[domain.Item]:
         """Find specific items for authorised user."""
-        # TODO(i.zyktin): need to implement this
-        return await self.search_specific_known(user, query, details)
+        _query = """
+        SELECT uuid,
+               parent_uuid,
+               owner_uuid,
+               number,
+               name,
+               is_collection,
+               content_ext,
+               preview_ext,
+               thumbnail_ext,
+               ct.tags
+        FROM items it
+                 RIGHT JOIN computed_tags ct ON ct.item_uuid = it.uuid
+                 RIGHT JOIN computed_permissions cp ON cp.item_uuid = it.uuid
+        WHERE :user_uuid = ANY(cp.permissions)
+          AND ct.tags @> :tags_include
+          AND NOT ct.tags && :tags_exclude
+        ORDER BY number LIMIT :limit OFFSET :offset;
+        """
+
+        values = {
+            'user_uuid': user.uuid,
+            'limit': details.items_per_page,
+            'offset': details.offset,
+            'tags_include': query.tags_include,
+            'tags_exclude': query.tags_exclude,
+        }
+
+        response = await self.db.fetch_all(_query, values)
+        return [domain.Item.from_map(row) for row in response]
