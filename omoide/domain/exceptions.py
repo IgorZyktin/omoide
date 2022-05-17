fb443b0c79b368020a1533521fcf051c1b3b9476
IgorZyktin <nicord@yandex.ru>
2022-05-17 22:29:17 +0300

    feat: added more custom exc

--- omoide/domain/exceptions.py
+++ omoide/domain/exceptions.py
@@ -9,3 +9,15 @@ class BaseOmoideException(Exception):
 
 class IncorrectUUID(BaseOmoideException):
     """Given UUID is not correct."""
+
+
+class NotFound(BaseOmoideException):
+    """Target resource does not exist."""
+
+
+class Unauthorized(BaseOmoideException):
+    """User must be logged in to get this resource."""
+
+
+class Forbidden(BaseOmoideException):
+    """User has no access to this resource."""
