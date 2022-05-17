700bea345a989c391fed8ef70358bfe461503718
IgorZyktin <nicord@yandex.ru>
2022-05-17 22:29:30 +0300

    feat: refined special handlers

--- omoide/presentation/application/special.py
+++ omoide/presentation/application/special.py
@@ -1,21 +1,26 @@
 # -*- coding: utf-8 -*-
 """Browse related routes.
 """
+import http
+
 import fastapi
 from fastapi.responses import HTMLResponse
 
-from omoide.presentation import dependencies, infra, constants
+from omoide import domain
+from omoide.presentation import dependencies as dep
+from omoide.presentation import infra, constants
 from omoide.presentation.config import config
-from omoide.domain import auth
+
 router = fastapi.APIRouter()
 
 
 @router.get('/not_found')
 async def not_found(
         request: fastapi.Request,
+        user: domain.User = fastapi.Depends(dep.get_current_user),
         response_class=HTMLResponse,
 ):
-    """Show Not found page."""
+    """Show <not found> page."""
     details = infra.parse.details_from_params(
         params=request.query_params,
         items_per_page=constants.ITEMS_PER_PAGE,
@@ -26,22 +31,23 @@ async def not_found(
     context = {
         'request': request,
         'config': config,
-        'user': auth.User.new_anon(),
+        'user': user,
         'query': infra.query_maker.QueryWrapper(query, details),
     }
-    return dependencies.templates.TemplateResponse(
-        name='not_found.html',
+    return dep.templates.TemplateResponse(
+        name='exc_not_found.html',
         context=context,
-        status_code=404,
+        status_code=http.HTTPStatus.NOT_FOUND,
     )
 
 
-@router.get('/not_allowed')
-async def not_allowed(
+@router.get('/unauthorized')
+async def unauthorized(
         request: fastapi.Request,
+        user: domain.User = fastapi.Depends(dep.get_current_user),
         response_class=HTMLResponse,
 ):
-    """Show Not allowed page."""
+    """Show <unauthorized> page."""
     details = infra.parse.details_from_params(
         params=request.query_params,
         items_per_page=constants.ITEMS_PER_PAGE,
@@ -52,22 +58,23 @@ async def not_allowed(
     context = {
         'request': request,
         'config': config,
-        'user': auth.User.new_anon(),
+        'user': user,
         'query': infra.query_maker.QueryWrapper(query, details),
     }
-    return dependencies.templates.TemplateResponse(
-        name='not_allowed.html',
+    return dep.templates.TemplateResponse(
+        name='exc_unauthorized.html',
         context=context,
-        status_code=401,
+        status_code=http.HTTPStatus.UNAUTHORIZED,
     )
 
 
-@router.get('/not_appropriate')
-async def not_appropriate(
+@router.get('/forbidden')
+async def forbidden(
         request: fastapi.Request,
+        user: domain.User = fastapi.Depends(dep.get_current_user),
         response_class=HTMLResponse,
 ):
-    """Show Not appropriate page."""
+    """Show <forbidden> page."""
     details = infra.parse.details_from_params(
         params=request.query_params,
         items_per_page=constants.ITEMS_PER_PAGE,
@@ -78,11 +85,38 @@ async def not_appropriate(
     context = {
         'request': request,
         'config': config,
-        'user': auth.User.new_anon(),
+        'user': user,
         'query': infra.query_maker.QueryWrapper(query, details),
     }
-    return dependencies.templates.TemplateResponse(
-        name='not_appropriate.html',
+    return dep.templates.TemplateResponse(
+        name='exc_forbidden.html',
         context=context,
-        status_code=400,
+        status_code=http.HTTPStatus.FORBIDDEN,
+    )
+
+
+@router.get('/bad_request')
+async def bad_request(
+        request: fastapi.Request,
+        user: domain.User = fastapi.Depends(dep.get_current_user),
+        response_class=HTMLResponse,
+):
+    """Show <bad request> page."""
+    details = infra.parse.details_from_params(
+        params=request.query_params,
+        items_per_page=constants.ITEMS_PER_PAGE,
+    )
+
+    query = infra.query_maker.from_request(request.query_params)
+
+    context = {
+        'request': request,
+        'config': config,
+        'user': user,
+        'query': infra.query_maker.QueryWrapper(query, details),
+    }
+    return dep.templates.TemplateResponse(
+        name='exc_bad_request.html',
+        context=context,
+        status_code=http.HTTPStatus.BAD_REQUEST,
     )
