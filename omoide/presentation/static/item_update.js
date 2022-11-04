69358f0f08c289a0c2aa783d660e623dac09e13d
Igor Zyktin <nicord@yandex.ru>
2022-11-04 13:42:24 +0300

    feat: no disabled fields on item edit

--- omoide/presentation/static/item_update.js
+++ omoide/presentation/static/item_update.js
@@ -15,83 +15,28 @@ const BASIC_FIELDS = BASIC_TEXT_FIELDS.concat(BASIC_BOOL_FIELDS)
 function alterModelTextField(element, fieldName) {
     // change item model field
     newModel[fieldName] = $(element).val().trim()
-    checkChangesBasic('save_basic')
 }
 
 
 function alterModelBoolField(element, fieldName) {
     // change item model field
     newModel[fieldName] = $(element).is(':checked')
-    checkChangesBasic('save_basic')
 }
 
 function alterModelParentField(element, fieldName) {
     // change item model field
     let value = $(element).val().trim()
     newModel[fieldName] = value || null
-    checkChangesParent('save_parent')
 }
 
 function alterModelTagsField(element) {
     // change item model field
     newModel['tags'] = splitLines($(element).val())
-    checkChangesTags('save_tags')
 }
 
 function alterModelPermissionsField(element) {
     // change item model field
-    newModel['permissions'] = [...$(element).val().matchAll(UUID_REGEXP)].flat()
-    checkChangesPermissions('save_permissions')
-}
-
-function checkChangesBasic(elementId) {
-    // toggle save button for basic fields
-    checkChanges(oldModel, newModel, elementId, BASIC_FIELDS, [])
-}
-
-function checkChangesParent(elementId) {
-    // toggle save button for basic fields
-    checkChanges(oldModel, newModel, elementId, ['parent_uuid'], [])
-}
-
-function checkChangesTags(elementId) {
-    // toggle save button for tags
-    checkChanges(oldModel, newModel, elementId, [], ['tags'])
-}
-
-function checkChangesPermissions(elementId) {
-    // toggle save button for permissions
-    checkChanges(oldModel, newModel, elementId, [], ['permissions'])
-}
-
-function checkChanges(oldModel, newModel, elementId, fields, arrayFields) {
-    // toggle save button for basic fields
-    let changed = false
-    let button = $('#' + elementId)
-
-    for (let field of fields) {
-        if (oldModel[field] !== newModel[field]) {
-            changed = true
-            break
-        }
-    }
-
-    if (!changed) {
-        for (let field of arrayFields) {
-            if (!arraysAreIdentical(oldModel[field], newModel[field])) {
-                changed = true
-                break
-            }
-        }
-    }
-
-    if (changed) {
-        button.removeClass('button-disabled')
-        button.prop('disabled', false);
-    } else {
-        button.addClass('button-disabled')
-        button.prop('disabled', true);
-    }
+    newModel['permissions'] = extractUUIDs($(element).val())
 }
 
 function resetBasic() {
@@ -105,21 +50,18 @@ function resetBasic() {
     $('#item_content_ext').val(oldModel['content_ext'] || '')
     $('#item_preview_ext').val(oldModel['preview_ext'] || '')
     $('#item_thumbnail_ext').val(oldModel['thumbnail_ext'] || '')
-    checkChangesBasic('save_basic')
 }
 
 function resetTags() {
     // restore parameters
     newModel['tags'] = oldModel['tags']
     fillTextarea('item_tags', oldModel['tags'])
-    checkChangesTags('save_tags')
 }
 
 function resetPermissions() {
     // restore parameters
     newModel['permissions'] = oldModel['permissions']
     fillTextarea('item_permissions', initialPermissions)
-    checkChangesPermissions('save_permissions')
 }
 
 function resetParent() {
@@ -127,7 +69,6 @@ function resetParent() {
     newModel['parent_uuid'] = oldModel['parent_uuid']
     $('#item_parent').val(oldModel['parent_uuid'] || '')
     tryLoadingThumbnail(oldModel['parent_uuid'], $('#thumbnail_parent'))
-    checkChangesParent('save_parent')
 }
 
 function fillTextarea(elementId, values) {
@@ -192,7 +133,6 @@ function saveBasic(alertsElementId) {
             for (let field of BASIC_FIELDS) {
                 oldModel[field] = newModel[field]
             }
-            checkChangesBasic('save_basic')
             tryLoadingThumbnail(oldModel['uuid'], $('#thumbnail'))
             makeAnnounce('Basic fields saved', alertsElementId)
         },
@@ -216,7 +156,6 @@ function saveParent(totalChildren, alertsElementId) {
         success: function (response) {
             console.log('Saved parent', response)
             oldModel['parent_uuid'] = newModel['parent_uuid']
-            checkChangesParent('save_parent')
             tryLoadingThumbnail(oldModel['parent_uuid'], $('#thumbnail_parent'))
             makeAnnounce('Parent changed', alertsElementId)
         },
@@ -244,7 +183,6 @@ function saveTags(totalChildren, alertsElementId) {
         success: function (response) {
             console.log('Saved tags', response)
             oldModel['tags'] = newModel['tags']
-            checkChangesTags('save_tags')
             makeAnnounce('Tags saved', alertsElementId)
         },
         error: function (XMLHttpRequest, textStatus, errorThrown) {
@@ -278,7 +216,6 @@ function savePermissions(totalChildren, alertsElementId) {
             console.log('Saved permissions', response)
             oldModel['permissions'] = newModel['permissions']
             initialPermissions = newModel['permissions']
-            checkChangesPermissions('save_permissions')
             makeAnnounce('Permissions saved', alertsElementId)
         },
         error: function (XMLHttpRequest, textStatus, errorThrown) {
