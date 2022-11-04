c63ac7b6e638d71c27568c982f093938bd9d4b80
Igor Zyktin <nicord@yandex.ru>
2022-11-04 20:05:53 +0300

    feat: renamed files

--- omoide/use_cases/__init__.py
+++ omoide/use_cases/__init__.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 from omoide.use_cases.api.uc_api_browse import *
+from omoide.use_cases.api.uc_api_exif import *
 from omoide.use_cases.api.uc_api_items import *
-from omoide.use_cases.api.uc_exif import *
-from omoide.use_cases.api.uc_media import *
-from omoide.use_cases.api.uc_metainfo import *
+from omoide.use_cases.api.uc_api_media import *
+from omoide.use_cases.api.uc_api_metainfo import *
+from omoide.use_cases.api.uc_api_users import *
 from omoide.use_cases.application.home import *
 from omoide.use_cases.application.uc_app_browse import *
 from omoide.use_cases.application.uc_app_items import *
