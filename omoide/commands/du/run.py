2b456efb53a145c0619928b85798e1ff560abe53
Igor Zyktin <nicord@yandex.ru>
2022-11-13 20:51:23 +0300

    feat: base for du

--- omoide/commands/du/run.py
+++ omoide/commands/du/run.py
@@ -1,3 +1,14 @@
 # -*- coding: utf-8 -*-
 """Disk Usage command.
 """
+from sqlalchemy.engine import Engine
+
+from omoide.commands.du.cfg import Config
+
+
+def main(
+        engine: Engine,
+        config: Config,
+):
+    """Show disk usage for every user."""
+    print(1)
