08b4820d3ca63c31c6ca80b82d4e5cd031f15208
Igor Zyktin <nicord@yandex.ru>
2022-12-02 19:34:39 +0300

    feat: worker init tune

--- run_worker_dev.sh
+++ run_worker_dev.sh
@@ -3,9 +3,7 @@ source venv/bin/activate
 python -m omoide.daemons.worker \
   --name "$OMOIDE_WORKER_NAME" \
   --db-url "$OMOIDE_DB_URL" \
-  --hot-folder "$OMOIDE_HOT_FOLDER" \
   --cold-folder "$OMOIDE_COLD_FOLDER" \
-  --save-hot \
   --save-cold \
   --download-media \
   --manual-copy \
