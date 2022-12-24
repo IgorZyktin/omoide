600fae0cda9afb38c482935c62614c42082770e9
Igor Zyktin <nicord@yandex.ru>
2022-12-24 14:55:49 +0300

    feat: altered db url for omoide-app

--- omoide/presentation/app_config.py
+++ omoide/presentation/app_config.py
@@ -1,15 +1,13 @@
 # -*- coding: utf-8 -*-
 """Application settings.
 """
-from typing import Optional
-
 from pydantic import BaseSettings
 from pydantic import SecretStr
 
 
 class Config(BaseSettings):
     """Application settings."""
-    db_url: SecretStr
+    db_url_app: SecretStr
     injection: str = ''
     env: str = 'dev'
     host: str = '0.0.0.0'
@@ -17,24 +15,3 @@ class Config(BaseSettings):
 
     class Config:
         env_prefix = 'omoide_'
-
-
-_active_config: Optional[Config] = None
-
-
-def init(given_config: Optional[Config] = None) -> Config:
-    """Generate new config instance."""
-    global _active_config
-
-    if given_config is None:
-        _active_config = Config()
-    else:
-        _active_config = given_config
-    return _active_config
-
-
-def get_config() -> Config:
-    """Get instance of active config."""
-    if _active_config is None:
-        raise RuntimeError('No active config')
-    return _active_config
