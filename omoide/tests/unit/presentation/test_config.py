f4b5c3291c4072ccf2b452476d690bc1dd9ea320
Igor Zyktin <nicord@yandex.ru>
2022-12-24 16:15:48 +0300

    fix: lazy config load in tests

--- omoide/tests/unit/presentation/test_config.py
+++ omoide/tests/unit/presentation/test_config.py
@@ -3,7 +3,6 @@
 """
 import os
 from unittest import mock
-from uuid import UUID
 
 import pytest
 
@@ -14,11 +13,7 @@ from omoide.presentation import app_config
 def ref_config():
     config = mock.Mock()
     config.env = 'test'
-    config.db_url = 'fake-db-url'
-    config.test_users = frozenset([
-        UUID('07c693ef-fb26-42f8-aea2-3275a45828bb'),
-        UUID('f4986873-c10a-4770-8059-3558add8846c'),
-    ])
+    config.db_url_app = 'fake-db-url'
     return config
 
 
@@ -26,8 +21,7 @@ def ref_config():
 def fake_env_for_config(ref_config):
     return {
         'OMOIDE_ENV': ref_config.env,
-        'OMOIDE_DB_URL': ref_config.db_url,
-        'OMOIDE_TEST_USERS': ','.join(map(str, ref_config.test_users)),
+        'OMOIDE_DB_URL_APP': ref_config.db_url_app,
     }
 
 
@@ -35,8 +29,8 @@ def test_config(fake_env_for_config, ref_config):
     """Must correctly parse env into config."""
     # act
     with mock.patch.dict(os.environ, fake_env_for_config):
-        config = app_config.init()
+        config = app_config.Config()
 
     # assert
-    assert config.db_url.get_secret_value() == ref_config.db_url
+    assert config.db_url_app.get_secret_value() == ref_config.db_url_app
     assert config.env == 'test'
