f5b84c887dd8b420e5940e667c381a09bad5b8a8
Igor Zyktin <nicord@yandex.ru>
2023-08-20 13:34:33 +0300

    feat: better worker config

--- omoide/tests/unit/daemons/worker/conftest.py
+++ omoide/tests/unit/daemons/worker/conftest.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 """Tests.
 """
 import datetime
@@ -7,7 +6,7 @@ import tempfile
 import pytest
 from pydantic import SecretStr
 
-from omoide.daemons.worker import cfg
+from omoide.daemons.worker import worker_config
 
 
 @pytest.fixture
@@ -15,45 +14,37 @@ def valid_worker_config_dict():
     with tempfile.TemporaryDirectory() as tmp_dir:
         yield dict(
             name='test',
-            db_url=SecretStr('test'),
+            db_uri=SecretStr('test'),
+            db_echo=False,
             hot_folder=tmp_dir,
-            cold_folder='',
+            cold_folder=None,
             save_hot=True,
             save_cold=False,
-            download_media=True,
-            manual_copy=True,
-            drop_done_media=False,
-            drop_done_copies=False,
-            min_interval=5,
-            max_interval=300,
-            warm_up_coefficient=25.4,
-            batch_size=15,
-            log_level='NOTSET',
-            debug=False,
+            log_level='INFO',
+            batch_size=5,
             prefix_size=3,
-            single_run=False,
-            echo=False,
-            replication_formula={'test-hot': True, 'test-cold': True},
-            _existing_folder=tmp_dir,
+            run_once=True,
+            media=dict(
+                should_process=True,
+                drop_after=True,
+                replication_formula={'test-hot': True, 'test-cold': True}
+            ),
+            manual_copy=dict(
+                should_process=True,
+                drop_after=True,
+            ),
+            timer_strategy=dict(
+                min_interval=5,
+                max_interval=300,
+                warm_up_coefficient=25.4,
+            ),
+            strategy='TimerStrategy',
         )
 
 
 @pytest.fixture
 def valid_worker_config(valid_worker_config_dict):
-    return cfg.Config(**valid_worker_config_dict)
-
-
-@pytest.fixture
-def valid_worker_config_argv():
-    with tempfile.TemporaryDirectory() as tmp_dir:
-        yield [
-            '--name', 'test',
-            '--db-url', 'test',
-            '--hot-folder', tmp_dir,
-            '--save-hot',
-            '--download-media',
-            '--replication-formula', '{"test-hot": true}'
-        ]
+    return worker_config.Config(**valid_worker_config_dict)
 
 
 @pytest.fixture
--- omoide/tests/unit/daemons/worker/test_worker_config.py
+++ omoide/tests/unit/daemons/worker/test_worker_config.py
@@ -1,27 +1,23 @@
-# -*- coding: utf-8 -*-
 """Tests.
 """
-from unittest import mock
-
+import pydantic
 import pytest
-from click.testing import CliRunner
 from pydantic import ValidationError
 
-from omoide.daemons.worker import cfg
-from omoide.daemons.worker.__main__ import main
+from omoide.daemons.worker.worker_config import Config
+from omoide.daemons.worker.worker_config import serialize
 
 
 def test_worker_config_correct(valid_worker_config_dict):
     """Must generate valid config."""
-    config = cfg.Config(**valid_worker_config_dict)
+    config = Config(**valid_worker_config_dict)
     assert config is not None
-    assert isinstance(config.verbose(), str)
 
 
 @pytest.mark.parametrize('hot_folder,cold_folder,save_hot,save_cold', [
-    ('', '', False, False),
-    ('/', '', False, True),
-    ('', '/', True, False),
+    (None, None, False, False),
+    ('/', None, False, True),
+    (None, '/', True, False),
 ])
 def test_worker_config_folders(
         valid_worker_config_dict,
@@ -37,16 +33,16 @@ def test_worker_config_folders(
     valid_worker_config_dict['save_cold'] = save_cold
 
     with pytest.raises(ValidationError):
-        cfg.Config(**valid_worker_config_dict)
+        Config(**valid_worker_config_dict)
 
 
 @pytest.mark.parametrize('min_interval', [0, 9999999999999])
 def test_worker_config_min_interval(valid_worker_config_dict, min_interval):
     """Must raise on inadequate min intervals."""
-    valid_worker_config_dict['min_interval'] = min_interval
+    valid_worker_config_dict['timer_strategy']['min_interval'] = min_interval
 
     with pytest.raises(ValidationError):
-        cfg.Config(**valid_worker_config_dict)
+        Config(**valid_worker_config_dict)
 
 
 @pytest.mark.parametrize('min_interval, max_interval', [
@@ -59,11 +55,11 @@ def test_worker_config_max_interval(
         max_interval,
 ):
     """Must raise on inadequate max intervals."""
-    valid_worker_config_dict['min_interval'] = min_interval
-    valid_worker_config_dict['max_interval'] = max_interval
+    valid_worker_config_dict['timer_strategy']['min_interval'] = min_interval
+    valid_worker_config_dict['timer_strategy']['max_interval'] = max_interval
 
     with pytest.raises(ValidationError):
-        cfg.Config(**valid_worker_config_dict)
+        Config(**valid_worker_config_dict)
 
 
 @pytest.mark.parametrize('warm_up_coefficient', [0.0, 9999999999999.0])
@@ -72,10 +68,11 @@ def test_worker_config_warm_up_coefficient(
         warm_up_coefficient,
 ):
     """Must raise on inadequate warm up coefficient."""
-    valid_worker_config_dict['warm_up_coefficient'] = warm_up_coefficient
+    valid_worker_config_dict['timer_strategy']['warm_up_coefficient'] \
+        = warm_up_coefficient
 
     with pytest.raises(ValidationError):
-        cfg.Config(**valid_worker_config_dict)
+        Config(**valid_worker_config_dict)
 
 
 @pytest.mark.parametrize('batch_size', [0, 9999999999999])
@@ -84,74 +81,87 @@ def test_worker_config_batch_size(valid_worker_config_dict, batch_size):
     valid_worker_config_dict['batch_size'] = batch_size
 
     with pytest.raises(ValidationError):
-        cfg.Config(**valid_worker_config_dict)
-
-
-def test_worker_is_allowed_to_save(valid_worker_config_dict):
-    """Must raise if user wants to save something but cannot."""
-    valid_worker_config_dict['media_downloading'] = False
-    valid_worker_config_dict['save_hot'] = True
-    valid_worker_config_dict['save_cold'] = True
-
-    with pytest.raises(ValidationError):
-        cfg.Config(**valid_worker_config_dict)
-
-
-def test_worker_config_generation(valid_worker_config_argv):
-    """Must create config instance from CLI args."""
-    with mock.patch('omoide.daemons.worker.__main__.run') as fake_run, \
-            mock.patch('omoide.daemons.worker.__main__.Database'):
-        runner = CliRunner()
-        result = runner.invoke(main, valid_worker_config_argv)  # noqa
-
-    assert result.exit_code == 0
-    fake_run.assert_called_once()
-
-
-def test_worker_config_interrupt(valid_worker_config_argv):
-    """Must stop on keyboard interrupt."""
-    with mock.patch('omoide.daemons.worker.__main__.run') as fake_run, \
-            mock.patch('omoide.daemons.worker.__main__.Database'):
-        fake_run.side_effect = KeyboardInterrupt
-        runner = CliRunner()
-        result = runner.invoke(main, valid_worker_config_argv)  # noqa
-
-    assert result.exit_code == 0
-    fake_run.assert_called_once()
+        Config(**valid_worker_config_dict)
 
 
 def test_worker_config_no_folders_exist(valid_worker_config_dict):
     """Must raise because no folder was found."""
-    valid_worker_config_dict['hot_folder'] = 'nonexistent'
-    valid_worker_config_dict['cold_folder'] = 'nonexistent'
+    valid_worker_config_dict['hot_folder'] = '/nonexistent'
+    valid_worker_config_dict['cold_folder'] = '/nonexistent'
 
     with pytest.raises(ValidationError, match='Both hot and cold folders*'):
-        cfg.Config(**valid_worker_config_dict)
+        Config(**valid_worker_config_dict)
 
 
 def test_worker_config_hot_does_not_exist(valid_worker_config_dict):
     """Must raise because hot folder is required and does not exist."""
-    existing = valid_worker_config_dict['_existing_folder']
-
     valid_worker_config_dict['save_hot'] = True
-    valid_worker_config_dict['hot_folder'] = 'nonexistent'
-
     valid_worker_config_dict['save_cold'] = False
-    valid_worker_config_dict['cold_folder'] = existing
+
+    valid_worker_config_dict['cold_folder'] \
+        = valid_worker_config_dict['hot_folder']
+    valid_worker_config_dict['hot_folder'] = '/nonexistent'
 
     with pytest.raises(ValidationError, match='Hot folder does not exist*'):
-        cfg.Config(**valid_worker_config_dict)
+        Config(**valid_worker_config_dict)
 
 
 def test_worker_config_cold_does_not_exist(valid_worker_config_dict):
     """Must raise because cold folder is required and does not exist."""
-    existing = valid_worker_config_dict['_existing_folder']
-
     valid_worker_config_dict['save_hot'] = False
-    valid_worker_config_dict['hot_folder'] = existing
-
     valid_worker_config_dict['save_cold'] = True
-    valid_worker_config_dict['cold_folder'] = 'nonexistent'
+
+    valid_worker_config_dict['cold_folder'] = '/nonexistent'
 
     with pytest.raises(ValidationError, match='Cold folder does not exist*'):
-        cfg.Config(**valid_worker_config_dict)
+        Config(**valid_worker_config_dict)
+
+
+def test_worker_config_both_folders_do_not_exist(valid_worker_config_dict):
+    """Must raise because no folder exists."""
+    valid_worker_config_dict['save_hot'] = True
+    valid_worker_config_dict['save_cold'] = True
+
+    valid_worker_config_dict['hot_folder'] = '/nonexistent'
+    valid_worker_config_dict['cold_folder'] = '/nonexistent'
+
+    with pytest.raises(ValidationError, match='Both *'):
+        Config(**valid_worker_config_dict)
+
+
+@pytest.fixture
+def config_like_model():
+    class SubNested(pydantic.BaseModel):
+        k: str = 'value'
+        f: dict = pydantic.Field(default={'something': 'else'})
+
+    class Nested(pydantic.BaseModel):
+        x: str = 'other'
+        y: dict = pydantic.Field(default={'key': 'value'})
+        sub_nested: SubNested = SubNested()
+
+    class Model(pydantic.BaseModel):
+        a: int = 1
+        b: str = 'var'
+        nested: Nested = Nested()
+
+    return Model()
+
+
+def test_worker_config_serialization(config_like_model):
+    """Must serialize properly."""
+    reference = """
+a=1
+b='var'
+nested:
+    x='other'
+    y={'key': 'value'}
+    sub_nested:
+        k='value'
+        f={'something': 'else'}
+    """.strip()
+
+    result = serialize(config_like_model,
+                       do_not_serialize=frozenset(('y', 'f')))
+
+    assert result == reference
