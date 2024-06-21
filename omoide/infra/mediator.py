fb741c40a5440fa19d4b3a26eed2d5ee7064ef64
Igor <nicord@yandex.ru>
2024-06-21 22:36:33 +0300

    feat: additional mediator type

---
+++ omoide/infra/mediator.py
@@ -0,0 +1,12 @@
+"""Class that ties all components together.
+"""
+from dataclasses import dataclass
+
+from omoide.domain import interfaces
+
+
+@dataclass()
+class Mediator:
+    """Class that ties all components together.
+    """
+    users_repository: interfaces.AbsUsersReadRepository
