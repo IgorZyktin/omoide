711fc48f700a44a84767daf12367cce1b0a1493c
Igor Zyktin <nicord@yandex.ru>
2024-07-10 08:09:50 +0300

    feat: added get_size function

--- omoide/utils.py
+++ omoide/utils.py
@@ -1,8 +1,8 @@
-"""Common utils.
-"""
+"""Common utils."""
 import datetime
 import functools
 import re
+import sys
 from itertools import zip_longest
 from typing import Any
 from typing import Callable
@@ -322,3 +322,33 @@ def split(string: str, separator: str = ',') -> list[str]:
         for raw in string.split(separator)
         if (clear := raw.strip())
     ]
+
+
+def get_size(obj, seen: set[int] | None = None) -> int:
+    """Recursively finds size of objects."""
+    size = sys.getsizeof(obj)
+
+    if seen is None:
+        seen = set()
+
+    obj_id = id(obj)
+
+    if obj_id in seen:
+        return 0
+
+    seen.add(obj_id)
+
+    if isinstance(obj, dict):
+        size += sum([get_size(v, seen) for v in obj.values()])
+        size += sum([get_size(k, seen) for k in obj.keys()])
+
+    elif hasattr(obj, '__dict__'):
+        size += get_size(obj.__dict__, seen)
+
+    elif (
+        hasattr(obj, '__iter__')
+        and not isinstance(obj, (str, bytes, bytearray))
+    ):
+        size += sum([get_size(i, seen) for i in obj])
+
+    return size
