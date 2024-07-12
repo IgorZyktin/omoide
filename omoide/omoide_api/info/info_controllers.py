300c7e611f91a7608951566c55086c4a080b62ea
Igor Zyktin <nicord@yandex.ru>
2024-07-12 22:57:31 +0300

    feat: added whoami endpoint

--- omoide/omoide_api/info/info_controllers.py
+++ omoide/omoide_api/info/info_controllers.py
@@ -1,8 +1,13 @@
 """Technical information about the API."""
+from typing import Annotated
+
 from fastapi import APIRouter
+from fastapi import Depends
 from fastapi import status
 
 from omoide import const
+from omoide import models
+from omoide.presentation import dependencies as dep
 
 info_router = APIRouter(prefix='/info', tags=['Info'])
 
@@ -15,3 +20,26 @@ info_router = APIRouter(prefix='/info', tags=['Info'])
 async def api_get_version():
     """Get current version of the API."""
     return {'version': const.VERSION}
+
+
+@info_router.get(
+    '/whoami',
+    status_code=status.HTTP_200_OK,
+    response_model=dict[str, str | None],
+)
+async def api_get_myself(
+    user: Annotated[models.User, Depends(dep.get_current_user)],
+):
+    """Return current user as API sees it."""
+    if user.is_anon:
+        result = {
+            'uuid': None,
+            'name': 'anon',
+        }
+    else:
+        result = {
+            'uuid': str(user.uuid),
+            'name': user.name,
+        }
+
+    return result
