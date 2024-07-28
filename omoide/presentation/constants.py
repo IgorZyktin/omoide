4f840522dc2c7611ea433dfc161c2435b61aa309
Igor Zyktin <nicord@yandex.ru>
2024-07-28 17:28:21 +0300

    feat: dropped unused constant

--- omoide/presentation/constants.py
+++ omoide/presentation/constants.py
@@ -1,7 +1,4 @@
-# -*- coding: utf-8 -*-
-"""Application-wide constants.
-"""
+"""Application-wide constants."""
 ITEMS_PER_PAGE = 25
 MAX_ITEMS_PER_PAGE = 100
-ITEMS_PER_UPLOAD = 25
 PAGES_IN_BLOCK = 10
