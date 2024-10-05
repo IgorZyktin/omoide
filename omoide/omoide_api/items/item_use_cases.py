49409406ced57314ec1e12cc91f79136228a6879
Igor Zyktin <nicord@yandex.ru>
2024-10-05 15:09:03 +0300

    fix: enable signature use

--- omoide/omoide_api/items/item_use_cases.py
+++ omoide/omoide_api/items/item_use_cases.py
@@ -346,9 +346,7 @@ class DownloadCollectionUseCase(BaseItemUseCase):
             f'{template.format(current)}___{item_uuid}.{content_ext}'
         )
 
-        # FIXME - temporary disable
-        # checksum = signature if signature is not None else '-'
-        checksum = '-'
+        checksum = hex(signature) if signature is not None else '-'
         size = metainfo.content_size or 0
 
         mod_zip_line = (
