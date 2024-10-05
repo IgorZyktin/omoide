b99c41526168465ad2accf88b6468969dabceea5
Igor Zyktin <nicord@yandex.ru>
2024-10-05 20:26:13 +0300

    feat: added tool for column move

---
+++ omoide/commands/move_metainfo_key/run.py
@@ -0,0 +1,56 @@
+"""Move metainfo key to extras."""
+
+from uuid import UUID
+
+from sqlalchemy.orm.attributes import flag_modified
+
+from omoide import custom_logging
+from omoide.storage.database import db_models
+from omoide.storage.database.sync_db import SyncDatabase
+
+LOG = custom_logging.get_logger(__name__)
+
+
+def run(database: SyncDatabase, key: str,
+        batch_size: int, limit: int) -> None:
+    """Move metainfo key to extras."""
+    last_seen: UUID | None = None
+    processed = 0
+    running = True
+
+    with database.start_session() as session:
+        while running:
+            query = session.query(
+                db_models.Metainfo,
+            ).filter(
+                getattr(db_models.Metainfo, key) != None  # noqa
+            ).order_by(
+                db_models.Metainfo.item_uuid
+            )
+
+            if last_seen is not None:
+                query = query.filter(
+                    db_models.Metainfo.item_uuid > last_seen
+                )
+
+            models = query.limit(batch_size).all()
+
+            if not models:
+                break
+
+            for metainfo in models:
+                LOG.info(f'Altered `{key}` for {metainfo.item_uuid}')
+                metainfo.extras[key] = getattr(metainfo, key)
+                setattr(metainfo, key, None)
+                flag_modified(metainfo, 'extras')
+                session.flush([metainfo])
+
+                last_seen = metainfo.item_uuid
+                processed += 1
+                if 0 < limit <= processed:
+                    running = False
+                    break
+
+        session.commit()
+
+    LOG.info('Altered {} rows', processed)
