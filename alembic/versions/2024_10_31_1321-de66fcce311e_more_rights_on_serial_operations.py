d37ea369bb2948b0613af55dfb12fab5e1ddb5f8
Igor Zyktin <nicord@yandex.ru>
2024-10-31 13:24:01 +0300

    feat: more rights on serial operations

---
+++ alembic/versions/2024_10_31_1321-de66fcce311e_more_rights_on_serial_operations.py
@@ -0,0 +1,26 @@
+"""more rights on serial operations
+
+Revision ID: de66fcce311e
+Revises: dfdb436852da
+Create Date: 2024-10-31 13:21:06.126981+03:00
+"""
+
+from typing import Sequence
+
+from alembic import op
+
+
+revision: str = 'de66fcce311e'
+down_revision: str | None = 'dfdb436852da'
+branch_labels: str | Sequence[str] | None = None
+depends_on: str | Sequence[str] | None = None
+
+
+def upgrade() -> None:
+    """Adding stuff."""
+    op.execute('GRANT INSERT ON serial_operations TO omoide_app;')
+
+
+def downgrade() -> None:
+    """Removing stuff."""
+    op.execute('REVOKE SELECT ON serial_operations FROM omoide_app;')
