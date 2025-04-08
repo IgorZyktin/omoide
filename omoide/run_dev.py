bdf9959b33142d357bde001abe399c4b2d1d2912
Igor Zyktin <nicord@yandex.ru>
2025-04-08 06:53:42 +0300

    feat: small styling

--- omoide/run_dev.py
+++ omoide/run_dev.py
@@ -14,21 +14,27 @@ from omoide import cfg
 from omoide.application import app
 
 
-def main() -> None:
-    """Entry point."""
+def setup_uvicorn_logging():
+    """Make uvicorn logs more verbose."""
     log_config = uvicorn.config.LOGGING_CONFIG
     log_config['formatters']['access']['fmt'] = '%(asctime)s - %(levelname)s - %(message)s'
     log_config['formatters']['default']['fmt'] = '%(asctime)s - %(levelname)s - %(message)s'
 
+
+def route_iter(current_app: FastAPI, mount: str = '') -> Iterator[dict[str, str]]:
+    """Iterate on all routes, including nested applications."""
+    for route in current_app.routes:
+        if isinstance(route, APIRoute):
+            yield {'path': mount + route.path, 'name': route.name}
+        elif isinstance(route, Mount) and isinstance(route.app, FastAPI):
+            yield from route_iter(route.app, mount=mount + route.path)
+
+
+def main() -> None:
+    """Entry point."""
     config = pu.from_env(cfg.Config, env_prefix='omoide_app')
 
-    def route_iter(current_app: FastAPI, mount: str = '') -> Iterator[dict[str, str]]:
-        """Iterate on all routes, including nested applications."""
-        for route in current_app.routes:
-            if isinstance(route, APIRoute):
-                yield {'path': mount + route.path, 'name': route.name}
-            elif isinstance(route, Mount) and isinstance(route.app, FastAPI):
-                yield from route_iter(route.app, mount=mount + route.path)
+    setup_uvicorn_logging()
 
     @app.get('/all_routes')
     def get_all_urls_from_request(request: Request) -> list[dict[str, str]]:
