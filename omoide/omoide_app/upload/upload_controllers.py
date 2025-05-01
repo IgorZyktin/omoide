8d88071b41eb683c347146ace0954f9cc41bd38f
Igor Zyktin <nicord@yandex.ru>
2025-05-01 21:11:26 +0300

    feat: new upload app endpoint

--- omoide/omoide_app/upload/upload_controllers.py
+++ omoide/omoide_app/upload/upload_controllers.py
@@ -21,10 +21,10 @@ from omoide.presentation import web
 app_upload_router = fastapi.APIRouter()
 
 
-@app_upload_router.get('/upload/{item_uuid}')
+@app_upload_router.get('/upload/{parent_uuid}')
 async def app_upload(  # noqa: PLR0913
     request: Request,
-    item_uuid: UUID,
+    parent_uuid: UUID,
     templates: Annotated[Jinja2Templates, Depends(dep.get_templates)],
     user: Annotated[models.User, Depends(dep.get_current_user)],
     mediator: Annotated[Mediator, Depends(dep.get_mediator)],
@@ -39,7 +39,7 @@ async def app_upload(  # noqa: PLR0913
     use_case = upload_use_cases.AppUploadUseCase(mediator)
 
     try:
-        item, users_with_permission = await use_case.execute(user, item_uuid)
+        item, users_with_permission = await use_case.execute(user, parent_uuid)
     except Exception as exc:
         return web.redirect_from_exc(request, exc)
 
@@ -49,8 +49,9 @@ async def app_upload(  # noqa: PLR0913
         'user': user,
         'aim_wrapper': aim_wrapper,
         'url': request.url_for('app_search'),
+        'endpoint': request.url_for('api_upload_item', parent_uuid=parent_uuid),
         'current_item': item,
         'users_with_permission': users_with_permission,
     }
 
-    return templates.TemplateResponse('upload.html', context)
+    return templates.TemplateResponse('item_upload.html', context)
