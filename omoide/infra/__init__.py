27dc73297c77a3bfd4386e2c98b3946cf9bff507
Igor Zyktin <i.zyktin@corp.mail.ru>
2025-11-25 18:11:13 +0300

    feat: dropped fs locator

--- omoide/infra/__init__.py
+++ omoide/infra/__init__.py
@@ -1,2 +1,1 @@
 from omoide.infra.implementations.authenticator import BcryptAuthenticator  # noqa: F401
-from omoide.infra.implementations.fs_locator import FilesystemLocator  # noqa: F401
--- omoide/infra/interfaces/__init__.py
+++ omoide/infra/interfaces/__init__.py
@@ -1,2 +1,1 @@
 from omoide.infra.interfaces.abs_authenticator import AbsAuthenticator  # noqa: F401
-from omoide.infra.interfaces.abs_locator import AbsLocator  # noqa: F401
