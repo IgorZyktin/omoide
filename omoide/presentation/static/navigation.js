01c7fad4e1cccaf38e243e1f2b98fbe9d95279dd
Igor Zyktin <nicord@yandex.ru>
2026-02-21 23:33:22 +0300

    feat: grayscale menu background

--- omoide/presentation/static/navigation.js
+++ omoide/presentation/static/navigation.js
@@ -8,18 +8,22 @@ const swipeThresholdY = 200;
 function openNav() {
     // Open side menu
     document.getElementById("navbar").classList.add("sidenav-open");
+    document.getElementById("opaque").style.display = "block";
+    document.getElementById("content").style.filter = "grayscale(100%)";
 }
 
 function closeNav() {
     // Close side menu
     document.getElementById("navbar").classList.remove("sidenav-open");
+    document.getElementById("opaque").style.display = "none";
+    document.getElementById("content").style.filter = "none";
 }
 
 function handleGesture() {
     // React on user gestures
     const swipeDistanceX = touchEndX - touchStartX;
     const swipeDistanceY = Math.abs(touchEndY - touchStartY);
-    let open = document.getElementById("navbar").classList.contains('sidenav-open')
+    let open = document.getElementById("navbar").classList.contains('sidenav-open');
 
     // Check if the menu is closed and the swipe is from the left edge
     if (!open && swipeDistanceX > swipeThresholdX && swipeDistanceY < swipeThresholdY) {
