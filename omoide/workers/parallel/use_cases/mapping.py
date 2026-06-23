f70d014179477bf396023b7018a7b07b2cda3af7
Igor Zyktin <nicord@yandex.ru>
2026-06-23 23:10:13 +0300

    feat: dropped delete from old parallel worker

--- omoide/workers/parallel/use_cases/mapping.py
+++ omoide/workers/parallel/use_cases/mapping.py
@@ -1,10 +1,7 @@
 """Map of name to use cases."""
 
 from omoide.workers.parallel.use_cases import copy_use_cases
-from omoide.workers.parallel.use_cases import deletion_use_cases
 
 NAMES_TO_USE_CASES = {
     'copy': copy_use_cases.CopyUseCase,
-    'hard_delete': deletion_use_cases.HardDeleteUseCase,
-    'soft_delete': deletion_use_cases.SoftDeleteUseCase,
 }
